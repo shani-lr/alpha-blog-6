755c23cb3c75e26d82f46c075174cf808f492af4
shani-lr <shandy1806@gmail.com>
2022-05-30 22:23:23 +0300

    Add article category association when creating articles from UI

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CreateCategoryTest < ActionDispatch::IntegrationTest
   setup do
     @admin_user = User.create(username: "johndoe", email: "johndoe@example.com", password: "password", admin: true)
-    sign_in_as(@admin_user)
+    sign_in_as(@admin_user) 
   end
 
   test "get new category form and create category" do
